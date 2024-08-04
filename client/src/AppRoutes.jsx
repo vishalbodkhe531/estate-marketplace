dfa23b8d13631b97b66b643bb254ce0c54add257
Vishal Bodkhe <vishalbodkhe@gmail.com>
2024-08-04 18:02:38 +0530

    start project

---
+++ client/src/AppRoutes.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import {
+  Navigate,
+  Route,
+  BrowserRouter as Router,
+  Routes,
+} from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+
+function AppRoutes() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="*" element={<Navigate to={"/"} />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default AppRoutes;
