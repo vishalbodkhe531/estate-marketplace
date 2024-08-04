e7a823a2b90b4ca4febe860ab114131dca0311fd
Vishal Bodkhe <vishalbodkhe@gmail.com>
2024-08-04 18:17:08 +0530

    header complited in fronted/client

--- client/src/AppRoutes.jsx
+++ client/src/AppRoutes.jsx
@@ -10,10 +10,12 @@ import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
+import Header from "./components/Header";
 
 function AppRoutes() {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/profile" element={<Profile />} />
